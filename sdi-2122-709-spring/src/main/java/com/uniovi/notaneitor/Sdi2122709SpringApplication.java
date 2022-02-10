2e33503625140d76f16066a2a435f26463565d4e
uo258352 <uo258352@uniovi.es>
2022-02-10 10:43:47 +0100

    SDI-2122-709-2.1-Create Spring project.

---
+++ sdi-2122-709-spring/src/main/java/com/uniovi/notaneitor/Sdi2122709SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.notaneitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122709SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122709SpringApplication.class, args);
+    }
+
+}
